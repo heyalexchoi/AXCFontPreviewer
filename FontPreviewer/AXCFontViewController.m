0b8baadad419055af23ba560cc5b95d6da0d2e3b
Alex Choi <heyalexchoi@gmail.com>
2014-07-30 16:15:46 -0400

    fonts are now sorted alphabtically and searchable

--- FontPreviewer/AXCFontViewController.m
+++ FontPreviewer/AXCFontViewController.m
@@ -8,9 +8,10 @@
 
 #import "AXCFontViewController.h"
 
-@interface AXCFontViewController () <UICollectionViewDataSource,UICollectionViewDelegate>
-@property (strong,nonatomic) NSMutableArray * fontFamilyNames;
+@interface AXCFontViewController () <UICollectionViewDataSource,UICollectionViewDelegate, UISearchBarDelegate>
+
 @property (strong,nonatomic) NSMutableArray * fontNames;
+@property (strong,nonatomic) NSArray * filteredNames;
 @end
 
 @implementation AXCFontViewController
@@ -29,23 +30,33 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     
-    self.fontFamilyNames = [NSMutableArray arrayWithArray:[UIFont familyNames]];
-    self.fontNames = [NSMutableArray new];
+    UISearchBar * searchBar = [[UISearchBar alloc] initWithFrame:CGRectMake(0, 20, self.view.frame.size.width, 20)];
+    searchBar.backgroundImage = [UIImage new]; // get rid of that dumbass gray background box
+    [searchBar sizeToFit];
+    searchBar.delegate = self;
+    [self.view addSubview:searchBar];
     
+    self.collectionView.frame = CGRectMake(0,searchBar.frame.size.height + searchBar.frame.origin.y,self.collectionView.frame.size.width,self.collectionView.frame.size.height-searchBar.frame.size.height);
 
-    
-    [self.fontFamilyNames enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop)
+    self.fontNames = [NSMutableArray new];
+
+    [[UIFont familyNames] enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop)
     {
         NSString * familyName = obj;
         [self.fontNames addObjectsFromArray:[UIFont fontNamesForFamilyName:familyName]];
     }];
     
+    [self.fontNames enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
+        NSLog(@"%@",obj);
+    }];
     
-    
-    
+    [self.fontNames sortUsingSelector:@selector(localizedCaseInsensitiveCompare:)];
+    self.filteredNames = [self.fontNames copy];
     
 
-    
+        // dismiss search keyboard on searchbar on main view tap
+    [self.view addGestureRecognizer:[[UITapGestureRecognizer alloc] initWithTarget:searchBar action:@selector(endEditing:)]];
+
 }
 
 - (void)didReceiveMemoryWarning
@@ -58,12 +69,11 @@
 
 -(NSInteger) collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
 {
-    return self.fontNames.count;
+    return self.filteredNames.count;
 }
 
 -(NSInteger) numberOfSectionsInCollectionView:(UICollectionView *)collectionView
 {
-///    return self.fontFamilyNames.count;
     return 1;
 }
 
@@ -71,7 +81,7 @@
 {
     UICollectionViewCell * cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"cellID" forIndexPath:indexPath];
     
-    NSString * fontName = [self.fontNames objectAtIndex:indexPath.item];
+    NSString * fontName = [self.filteredNames objectAtIndex:indexPath.item];
     
     [cell.contentView.subviews enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop)
     {
@@ -106,15 +116,25 @@
         if ([subview isMemberOfClass:[UILabel class]])
         {
             UILabel * label = subview;
-            
-            
-            
         }
     }];
     
+
     return sectionHeader;
 }
 
+#pragma mark UISearchBarDelegate
+-(void) searchBar:(UISearchBar *)searchBar textDidChange:(NSString *)searchText
+{
+    if ([searchText isEqualToString:@""])
+    {
+        self.filteredNames = self.fontNames;
+        [self.collectionView reloadData];
+        return;
+    }
+    self.filteredNames = [self.fontNames filteredArrayUsingPredicate:[NSPredicate predicateWithFormat:@"SELF CONTAINS[cd] %@", searchText]];
+    [self.collectionView reloadData];
+}
 
 /*
 #pragma mark - Navigation
